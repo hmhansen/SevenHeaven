41e6938897d8c0ef9be780b88ca95ff34b210457
snifty <aleksanderne1@gmail.com>
2015-12-06 16:08:32 +0100

    masse greier

---
+++ flyplass.js
@@ -0,0 +1,6 @@
+			localStorage.setItem('Gardermoen');
+
+			var flyplass = localStorage.getItem('Gardermoen');
+			
+			localStorage.removeItem('Gardermoen');
+			var flyplass = localStorage.getItem('Gardermoen');
